e65f70ce45e2c6e47f3bc8379d25354c51b00ae2
Guiillermo Enrique <gsanchez1687@gmail.com>
2024-08-05 18:51:55 -0500

    Estableciendo por defecto a todos los campos a 200

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+        //Establecer el tamaño de los campos de las migraciones por defecto
+        Schema::defaultStringLength(200);
     }
 }
