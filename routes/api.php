304843b005e31fb37df42b4585b272228d3d8a16
Guiillermo Enrique <gsanchez1687@gmail.com>
2024-08-05 18:08:38 -0500

    Endpoint personalizados con Only y Except

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,20 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::resource('buyers', 'Buyer\BuyerController');
+//Buyers
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+//Category
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+
+//Products
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
+//Transactions
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+//Sellers
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+
+//Users
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
